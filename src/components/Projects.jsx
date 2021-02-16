f592f944c4efbc0bae77ec9999a352142524f807
beher <beher@192.168.0.30>
2021-02-15 18:47:39 -0600

    updated projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -17,7 +17,7 @@ export default class Projects extends Component {
 								<div className="project" style={{ backgroundImage: 'url(images/img-5.jpg)' }}>
 									<div className="desc">
 										<div className="con">
-											<h3><a href="https://github.com/DeepakSethy">Web App for Raise the Barr</a></h3>
+											<h3><a href="https://github.com/DeepakSethy">Angular App</a></h3>
 											<span></span>
 											<p className="icon">
 												<span><a href="https://github.com/DeepakSethy"><i className="icon-share3" /></a></span>
@@ -30,12 +30,10 @@ export default class Projects extends Component {
 								<div className="project" style={{ backgroundImage: 'url(images/img-3.jpg)' }}>
 									<div className="desc">
 										<div className="con">
-											<h3><a href="https://github.com/DeepakSethy">Fetch API in React Example</a></h3>
-											<span>Dave Ceddia React Tutorial (Replaced Axios with Fetch)</span>
+											<h3><a href="https://github.com/DeepakSethy">User Identity MicroServices</a></h3>
+											<span>Spring Boot,Auth0, JWT </span>
 											<p className="icon">
 												<span><a href="https://github.com/DeepakSethy"><i className="icon-share3" /></a></span>
-												{/* 	<span><a href="#"><i className="icon-eye" /> 100</a></span>
-												<span><a href="#"><i className="icon-heart" /> 49</a></span> */}
 											</p>
 										</div>
 									</div>
@@ -45,9 +43,8 @@ export default class Projects extends Component {
 								<div className="project" style={{ backgroundImage: 'url(images/img-6.jpg)' }}>
 									<div className="desc">
 										<div className="con">
-											<h3><a href="https://github.com/DeepakSethy">Coding Challenge: <br />DarkSky API</a></h3>
-											<span>Gives weather forecast of upcoming 7 days for any location using google's geolocation API.
-												<br />Weather data is fetched using Darksky API. It also gives a chart of  weather history for that particular location</span>
+											<h3><a href="https://github.com/DeepakSethy">Coding Challenge: <br /> API</a></h3>
+											<span>AWS Amplify Serverless Application and Azure Serverless Apps</span>
 											<p className="icon">
 												<span><a href="https://github.com/DeepakSethy"><i className="icon-share3" /></a></span>
 
